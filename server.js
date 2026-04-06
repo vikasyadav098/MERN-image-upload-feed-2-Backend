9d3e2b7a5e4a6e559bdc74883d536e2fc0844bed
Vikas Yadav <242939047+vikasyadav098@users.noreply.github.com>
2026-04-06 13:44:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
-const app=require('./src/app')
-
-
-const connectDB=require('./src/DB/db')
+const app = require('./src/app');
+const connectDB = require('./src/DB/db');
 
 connectDB();
 
-app.listen(3000,()=>{
-    console.log("server is running at port 3000");
-    
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
